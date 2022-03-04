282a0c1fc3f63bacc5a3e362cbd15129ad13c763
DiegoAxe <100392580+DiegoAxe@users.noreply.github.com>
2022-03-04 08:03:39 -0600

    Update: Comentariar el JS

--- Diversion/js/script.js
+++ Diversion/js/script.js
@@ -1,17 +1,21 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// "n" es el valor que recibe al presionar los botones, el valor o es "1" o es "-1"
+// "i" es el contador.
+// "_SlideActual" es la imagen que se debera presentar en dicho momento.
+
+var slideIndex = 1; 
+showDivs(slideIndex); 
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs(slideIndex += n); 
 }
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var _SlideActual = document.getElementsByClassName("mySlides");
+  if (n > _SlideActual.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = _SlideActual.length}
+  for (i = 0; i < _SlideActual.length; i++) {
+    _SlideActual[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  _SlideActual[slideIndex-1].style.display = "block";  
 }
